9470ac44ec69b0532d3f8e8670071647b967b15b
Kazuya-Sakashita <kz0508@gmail.com>
2023-08-22 07:31:38 +0900

    残り日数表示

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :purchase_date, presence: true
   validates :expiration_date, presence: true
+
+  def days_until_expiration
+    (expiration_date - Date.today).to_i
+  end
 end
