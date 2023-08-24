4040cc3e2ccdd328f04c151030460df8c0a7c167
Kazuya-Sakashita <kz0508@gmail.com>
2023-08-24 19:16:58 +0900

    ページネーション追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
     before_action :set_product, only: [:show, :edit, :update, :destroy]
   
     def index
-      @products = Product.all
+      @products = Product.all.page(params[:page]).per(10)
       @soon_expiring_count = @products.select { |p| (p.expiration_date - Date.today).to_i < 7 }.count
     end
   
